9b39740cf704bca68ac12324c739a932fa13be2d
Mohamed Amer <mohamedamerjalal@gmail.com>
2024-09-25 00:51:51 +0300

    update ComposeQuadrant 3rd

--- app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -5,12 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -44,35 +43,49 @@ class MainActivity : ComponentActivity() {
 fun ComposeQuadrantApp(
     modifier: Modifier = Modifier
 ) {
-    Column(modifier.fillMaxHeight()) {
-        Row(Modifier.weight(1f)) {
-            ComposableInfoCard(
-                title = stringResource(R.string.first_title),
-                description = stringResource(R.string.first_description),
-                backgroundColor = Color(0xFFEADDFF),
-                modifier = Modifier.weight(1f)
-            )
-            ComposableInfoCard(
-                title = stringResource(R.string.second_title),
-                description = stringResource(R.string.second_description),
-                backgroundColor = Color(0xFFD0BCFF),
-                modifier = Modifier.weight(1f)
-            )
-        }
-        Row(Modifier.weight(1f)) {
-            ComposableInfoCard(
-                title = stringResource(R.string.third_title),
-                description = stringResource(R.string.third_description),
-                backgroundColor = Color(0xFFB69DF8),
-                modifier = Modifier.weight(1f)
-            )
-            ComposableInfoCard(
-                title = stringResource(R.string.fourth_title),
-                description = stringResource(R.string.fourth_description),
-                backgroundColor = Color(0xFFF6EDFF),
-                modifier = Modifier.weight(1f)
-            )
-        }
+    BoxWithConstraints(modifier.fillMaxSize()) {
+        val boxHeight = maxHeight / 2
+        val boxWidth = maxWidth / 2
+
+        // First quadrant
+        ComposableInfoCard(
+            title = stringResource(R.string.first_title),
+            description = stringResource(R.string.first_description),
+            backgroundColor = Color(0xFFEADDFF),
+            modifier = Modifier
+                .size(boxWidth, boxHeight)
+                .align(Alignment.TopStart)
+        )
+
+        // Second quadrant
+        ComposableInfoCard(
+            title = stringResource(R.string.second_title),
+            description = stringResource(R.string.second_description),
+            backgroundColor = Color(0xFFD0BCFF),
+            modifier = Modifier
+                .size(boxWidth, boxHeight)
+                .align(Alignment.TopEnd)
+        )
+
+        // Third quadrant
+        ComposableInfoCard(
+            title = stringResource(R.string.third_title),
+            description = stringResource(R.string.third_description),
+            backgroundColor = Color(0xFFB69DF8),
+            modifier = Modifier
+                .size(boxWidth, boxHeight)
+                .align(Alignment.BottomStart)
+        )
+
+        // Fourth quadrant
+        ComposableInfoCard(
+            title = stringResource(R.string.fourth_title),
+            description = stringResource(R.string.fourth_description),
+            backgroundColor = Color(0xFFF6EDFF),
+            modifier = Modifier
+                .size(boxWidth, boxHeight)
+                .align(Alignment.BottomEnd)
+        )
     }
 }
 
@@ -83,18 +96,20 @@ private fun ComposableInfoCard(
     backgroundColor: Color,
     modifier: Modifier = Modifier
 ) {
-    Column(
+    BoxWithConstraints(
         modifier = modifier
             .fillMaxHeight()
             .background(backgroundColor)
             .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
+        contentAlignment = Alignment.Center
     ) {
+        val boxHeight = maxHeight / 2
+        val boxWidth = maxWidth / 1
         ComposeText(
             text = title,
-            modifier = Modifier.padding(bottom = 16.dp),
-            fontWeight = FontWeight.Bold
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(bottom = 16.dp).size(boxWidth, boxHeight).align(Alignment.Center),
         )
         ComposeText(
             text = description,
